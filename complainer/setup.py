4dcf9cf949ed7f12e10cb37d7a6547e609c2f7d5
Chris Wallace <cjwallace@cloudera.com>
2019-09-09 14:33:52 +0000

    Add setup.py to make complainer pip installable

---
+++ complainer/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Complainer',
+    version='1.0.0',
+    url='https://github.com/cjwallace/moaney.git',
+    author='Chris J. Wallace',
+    author_email='cjwallace@cloudera.com',
+    description='Complainer - a mortgage complaint app from Moaney',
+    packages=find_packages()
+)
